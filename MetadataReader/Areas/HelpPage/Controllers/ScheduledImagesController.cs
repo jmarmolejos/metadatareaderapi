fd5ed75d173ead67923fcd7c0d67b8b3a21101b9
Jose Marmolejos <marmolejos.jose@gmail.com>
2015-10-30 17:03:13 -0400

    Add details page.

---
+++ MetadataReader/Areas/HelpPage/Controllers/ScheduledImagesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using MetadataReader.Models;
+
+namespace MetadataReader.Areas.HelpPage.Controllers
+{
+    public class ScheduledImagesController : Controller
+    {
+        private IMetadataContext _context;
+
+        public ScheduledImagesController(IMetadataContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Show(int id)
+        {
+            var img = _context.ScheduledImages.Include(s => s.MetadataTags).FirstOrDefault(s => s.Id == id);
+            return View(img);
+        }
+    }
+}
