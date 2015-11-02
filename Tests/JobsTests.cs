a61de9d8f9e3f5b46542de779bc9ea96dd33cae5
Jose Marmolejos <marmolejos.jose@gmail.com>
2015-11-02 17:47:13 -0400

    Discovered Mock.Of<> and Mock.Get. ZOMG!

--- Tests/JobsTests.cs
+++ Tests/JobsTests.cs
@@ -15,36 +15,35 @@ namespace Tests
     [TestFixture]
     public class JobsTests
     {
-        private Mock<IMetadataContext> _context;
+        private IMetadataContext _context;
+        private DbSet<ScheduledImage> _scheduledImages;
 
         [SetUp]
         public void Setup()
         {
-            var scheduledImages = new Mock<DbSet<ScheduledImage>>();
-            _context = new Mock<IMetadataContext>();
-            _context.Setup(ctx => ctx.ScheduledImages).Returns(scheduledImages.Object);
+            _context = Mock.Of<IMetadataContext>();
+            _scheduledImages = Mock.Of<DbSet<ScheduledImage>>();
+
+            Mock.Get(_scheduledImages).Setup(si => si.Find(It.IsAny<int>())).Returns(new ScheduledImage());
+            Mock.Get(_context).Setup(c => c.ScheduledImages).Returns(_scheduledImages);
         }
 
         [Test]
         public void DownloadAndReadMetadata_Should_get_scheduled_image_from_db_and_process_it()
         {
             // Arrange
-            var scheduledImages = new Mock<DbSet<ScheduledImage>>();
-            scheduledImages.Setup(si => si.Find(It.IsAny<int>())).Returns(new ScheduledImage());
-            _context.Setup(ctx => ctx.ScheduledImages).Returns(scheduledImages.Object);
-
             var downloader = new Mock<IDownloadToStream>();
 
             var metadataReader = new Mock<ICustomMetadataReader>();
             metadataReader.Setup(m => m.ReadFromStream(It.IsAny<MemoryStream>())).Returns(new List<ImageMetadataTag>());
 
-            var jobsHelper = new JobsHelper(_context.Object, downloader.Object, metadataReader.Object);
+            var jobsHelper = new JobsHelper(_context, downloader.Object, metadataReader.Object);
 
             // Act
             jobsHelper.DownloadAndReadMetadata(1);
 
             // Assert
-            scheduledImages.Verify(x => x.Find(It.Is<int>(id => id == 1)));
+            Mock.Get(_scheduledImages).Verify(x => x.Find(It.Is<int>(id => id == 1)));
             downloader.Verify(x => x.Download(It.IsAny<string>()));
             metadataReader.Verify(x => x.ReadFromStream(It.IsAny<MemoryStream>()));
         }
--- Tests/SchedulerTests.cs
+++ Tests/SchedulerTests.cs
@@ -18,41 +18,40 @@ namespace Tests
     [TestFixture]
     public class SchedulerTests
     {
-        private Mock<IBackgroundJobClient> _client;
-        private Mock<IMetadataContext> _context;
+        private IBackgroundJobClient _client;
+        private IMetadataContext _context;
+        private DbSet<ScheduledImage> _scheduledImages;
 
         [SetUp]
         public void Setup()
         {
-            _client = new Mock<IBackgroundJobClient>();
+            _client = Mock.Of<IBackgroundJobClient>();
 
-            _context = new Mock<IMetadataContext>();
-            var scheduledImages = new Mock<DbSet<ScheduledImage>>().Object;
-            var imageMetadataTags = new Mock<DbSet<ImageMetadataTag>>().Object;
-            _context.Setup(ctx => ctx.ScheduledImages).Returns(scheduledImages);
-            _context.Setup(ctx => ctx.ImageMetadataTags).Returns(imageMetadataTags);
+            _context = Mock.Of<IMetadataContext>();
+            _scheduledImages = Mock.Of<DbSet<ScheduledImage>>();
+            var imageMetadataTags = Mock.Of<DbSet<ImageMetadataTag>>();
+            Mock.Get(_context).Setup(ctx => ctx.ScheduledImages).Returns(_scheduledImages);
+            Mock.Get(_context).Setup(ctx => ctx.ImageMetadataTags).Returns(imageMetadataTags);
         }
 
         [Test]
         public void SchedulerController_Should_start_background_process_on_post()
         {
             // Arrange
-            var scheduledImages = new Mock<DbSet<ScheduledImage>>();
-            scheduledImages
+            Mock.Get(_scheduledImages)
                 .Setup(si => si.Add(It.IsAny<ScheduledImage>()))
                 .Callback<ScheduledImage>((img) =>
                 {
                     img.Id = 3;
                 });
-            _context.Setup(ctx => ctx.ScheduledImages).Returns(scheduledImages.Object);
 
-            var controller = new SchedulerController(_context.Object, _client.Object);
+            var controller = new SchedulerController(_context, _client);
 
             // Act
             controller.Post(new AssetApiModel() {FileName = "foo", Url = "bar"});
             
             // Assert
-            _client.Verify(x => x.Create(
+            Mock.Get(_client).Verify(x => x.Create(
                 It.Is<Job>(job => job.Method.Name == "DownloadAndReadMetadata" && Convert.ToInt32(job.Args[0]) == 3),
                 It.IsAny<EnqueuedState>()));
         }
@@ -61,10 +60,7 @@ namespace Tests
         public void SchedulerController_Should_save_scheduled_image_entity()
         {
             // Arrange
-            var scheduledImages = new Mock<DbSet<ScheduledImage>>();
-            _context.Setup(ctx => ctx.ScheduledImages).Returns(scheduledImages.Object);
-
-            var controller = new SchedulerController(_context.Object, _client.Object);
+            var controller = new SchedulerController(_context, _client);
 
             // Act
             controller.Post(new AssetApiModel()
@@ -74,8 +70,8 @@ namespace Tests
             });
 
             // Assert
-            scheduledImages.Verify(x => x.Add(It.Is<ScheduledImage>(img => img.FileName == "foo")), "Filename is different from expected.");
-            scheduledImages.Verify(x => x.Add(It.Is<ScheduledImage>(img => img.CreatedDate != DateTime.MinValue)), "Date was not set.");
+            Mock.Get(_scheduledImages).Verify(x => x.Add(It.Is<ScheduledImage>(img => img.FileName == "foo")), "Filename is different from expected.");
+            Mock.Get(_scheduledImages).Verify(x => x.Add(It.Is<ScheduledImage>(img => img.CreatedDate != DateTime.MinValue)), "Date was not set.");
         }
 
         [Test]
